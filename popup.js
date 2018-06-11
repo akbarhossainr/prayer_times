f753c8337600c4519ffac87abde370bf750d72f7
akbarhossainr <akbarhossain15@gmail.com>
2018-06-11 14:11:49 +0600

    initial commit

---
+++ popup.js
@@ -0,0 +1,30 @@
+// get current location
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        console.log("Geolocation is not supported by this browser");
+    }
+}
+
+function showPosition(position) {
+	showPrayTimes(position.coords.latitude, position.coords.longitude, prayTimes);
+}
+
+function showPrayTimes(lat, long, prayTimes) {
+	var prayTimes = prayTimes.getTimes(new Date(), [lat, long], +6, 0, '12h');
+
+	var list = ['Fajr', 'Sunrise', 'Dhuhr', 'Asr', 'Maghrib', 'Isha', 'Midnight'];
+
+	var html = '<table id="timetable" class="table table-bordered table-hover">';
+	html += '<tr><th colspan="2">'+ new Date().toLocaleString()+ '</th></tr>';
+	for(var i in list)	{
+		html += '<tr><td>'+ list[i]+ '</td>';
+		html += '<td class="text-right">'+ prayTimes[list[i].toLowerCase()]+ '</td></tr>';
+	}
+	html += '</table>';
+
+	document.getElementById('divShowPrayTimes').innerHTML = html;
+}
+
+getLocation();
