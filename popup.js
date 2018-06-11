a6690c29968fe409e25702ee8f14717f858051e6
akbarhossainr <akbarhossain15@gmail.com>
2018-06-11 16:12:08 +0600

    working on get current waqt

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+// moment.locale('bn');
+
 // get current location
 function getLocation() {
     if (navigator.geolocation) {
@@ -16,11 +18,26 @@ function showPrayTimes(lat, long, prayTimes) {
 	var paryerTimes = prayTimes.getTimes(new Date(), [lat, long], +6, 0, '12h');
 
 	var list = ['Fajr', 'Sunrise', 'Dhuhr', 'Asr', 'Sunset', 'Maghrib', 'Isha', 'Midnight'];
-
 	var html = '<table id="timetable" class="table table-bordered table-hover">';
-	html += '<tr><th colspan="2">'+ new Date().toLocaleString()+ '</th></tr>';
+	html += '<tr><th colspan="2">'+moment().format('LLLL')+ '</th></tr>';
+
+
 	for(var i in list)	{
-		html += '<tr><td>'+ list[i]+ '</td>';
+		var currentWaqt = '';
+
+		var startTime = moment(paryerTimes[list[i].toLowerCase()], 'hh:mm a');
+		var endTime = moment(paryerTimes[list[i].toLowerCase()], 'hh:mm a');
+
+		console.log(startTime, endTime);
+		if (currentWaqt == '') {
+			if (moment().isBetween(startTime, endTime)) {
+				currentWaqt = 'bg-primary';
+			}
+		}
+
+		console.log(currentWaqt);
+
+		html += '<tr class="'+currentWaqt+'"><td>'+ list[i]+ '</td>';
 		html += '<td class="text-right">'+ paryerTimes[list[i].toLowerCase()]+ '</td></tr>';
 	}
 	html += '</table>';
