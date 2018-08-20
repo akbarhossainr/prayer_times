8f2f63cbf60900238b871be6e047c830a587c56c
akbarhossain <akbar.hossain@shohoz.net>
2018-08-20 12:35:14 +0600

    Bangla language support added

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,40 @@
 // moment.locale('bn');
 
+function en_to_bn_number_conversion(en_number) {
+	var bn_number = '';
+
+	for (var i = 0; i < en_number.length; i++) {
+
+		if (en_number[i] == '0') bn_number = bn_number + "০";
+
+		if (en_number[i] == '1') bn_number = bn_number + "১";
+
+		if (en_number[i] == '2') bn_number = bn_number + "২";
+
+		if (en_number[i] == '3') bn_number = bn_number + "৩";
+
+		if (en_number[i] == '4') bn_number = bn_number + "৪";
+
+		if (en_number[i] == '5') bn_number = bn_number + "৫";
+
+		if (en_number[i] == '6') bn_number = bn_number + "৬";
+
+		if (en_number[i] == '7') bn_number = bn_number + "৭";
+
+		if (en_number[i] == '8') bn_number = bn_number + "৮";
+
+		if (en_number[i] == '9') bn_number = bn_number + "৯";
+
+		if (en_number[i] == ':') bn_number = bn_number + ":";
+		
+		if (en_number[i] == 'am') bn_number = bn_number + "asjg";
+
+	}
+
+	return bn_number; 
+}
+
+
 // get current location
 function getLocation() {
     if (navigator.geolocation) {
@@ -21,11 +56,17 @@ function showPosition(position) {
 }
 
 function showPrayTimes(lat, long, prayTimes) {
+
+	// Method: University of Islamic Sciences, Karachi
+	// Abbr.: Karachi
+	// Region Used: Pakistan, Afganistan, Bangladesh, India
 	var prayerTimes = prayTimes.setMethod('Karachi');
 	var prayerTimes = prayTimes.adjust( {asr: 'Hanafi', maghrib: '3 min'} );
 	var prayerTimes = prayTimes.getTimes(new Date(), [lat, long, 300], +6, 0, '12h');
 
 	var list = ['Fajr', 'Sunrise', 'Dhuhr', 'Asr', 'Sunset', 'Maghrib', 'Isha', 'Midnight'];
+	var listBN = ['ফজর', 'সূর্যোদয়', 'জোহর', 'আসর', 'সূর্যাস্ত', 'মাগরিব', 'ইশা'];
+
 	var html = '<table id="timetable" class="table table-bordered table-hover">';
 	var nextPrayerTime;
 	var nextPrayerTimeRemaining;
@@ -51,7 +92,7 @@ function showPrayTimes(lat, long, prayTimes) {
 		if (currentWaqt == '') {
 			if (moment().isBetween(startTime, endTime)) {
 				if (list[i] != 'Sunrise' && list[i] != 'Sunset' && list[i] != 'Midnight') {
-					currentWaqt = 'bg-primary';
+					currentWaqt = 'bg-success';
 				}
 
 				nextPrayerTime = moment(prayerTimes[list[i+1].toLowerCase()], 'hh:mm a');
@@ -67,17 +108,18 @@ function showPrayTimes(lat, long, prayTimes) {
 					nextPrayerTime = moment(nextPrayerTime, 'hh:mm a');
 				}
 
-				nextPrayerTimeRemaining = nextPrayerTime.diff(moment());
-				if (moment(nextPrayerTimeRemaining).hour() > 0) {
-					nextPrayerTimeRemaining = moment(nextPrayerTimeRemaining).utc().format('HH:mm:ss');
-				}else{
-					nextPrayerTimeRemaining = moment(nextPrayerTimeRemaining).utc().format('mm:ss');
-				}
+				nextPrayerTimeRemaining = moment(nextPrayerTime.diff(moment())).utc().format('HH:mm:ss');
 			}
 		}
+		var waqt_name = list[i];
+		var paryer_time = prayerTimes[list[i].toLowerCase()];
+		if($("input[name='lang']:checked").val() === 'bn'){
+			paryer_time = en_to_bn_number_conversion(prayerTimes[list[i].toLowerCase()]);
+			waqt_name = listBN[i];
+		}
 
-		html += '<tr class="'+currentWaqt+'"><td>'+ list[i]+ '</td>';
-		html += '<td class="text-right">'+ prayerTimes[list[i].toLowerCase()]+ '</td></tr>';
+		html += '<tr class="'+currentWaqt+'"><td>'+waqt_name+ '</td>';
+		html += '<td class="text-right">'+paryer_time+ '</td></tr>';
 
 	}
 	html += '<tfoot><tr class="text-center"><td colspan="2"><strong>Next Prayer Time</strong><br> '+nextPrayerTimeName+' '+nextPrayerTimeRemaining+'</td></tfoot>';
@@ -87,10 +129,10 @@ function showPrayTimes(lat, long, prayTimes) {
 	document.getElementById('divShowPrayTimes').innerHTML = html;
 }
 
-var interval = setInterval(getLocation, 100);
+setInterval(getLocation, 100);
 
 // Set current date and time on card header
 function setCurrentDateTime() {
 	document.getElementById("currentDateTime").innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
 }
-var interval = setInterval(setCurrentDateTime, 100);
+setInterval(setCurrentDateTime, 100);
